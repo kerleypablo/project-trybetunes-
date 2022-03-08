39757b1d8ffe7c5bf58da77ff1fcf0d035391931
kerley <kerley.kls@gmail.com>
2022-03-08 14:52:46 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import Header from './Header';
+import './Search.css';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      btmHabilitado: true,
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange({ target: { value } }) {
+    const MIN_LENGHT = 2;
+    if (value.length >= MIN_LENGHT) {
+      this.setState({ btmHabilitado: false });
+    }
+  }
+
   render() {
+    const { btmHabilitado } = this.state;
+    const { onInputChange } = this;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <div className="boxSearch">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ onInputChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ btmHabilitado }
+          >
+            Procurar
+          </button>
+        </div>
       </div>
     );
   }
