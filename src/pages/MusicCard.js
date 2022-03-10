ef12e17e75aceaa088f8d9a2c1274c726f5118ce
kerley <kerley.kls@gmail.com>
2022-03-09 21:17:13 -0300

    requisito 8

--- src/pages/MusicCard.js
+++ src/pages/MusicCard.js
@@ -1,10 +1,40 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 import './MusicCard.css';
 
 class MusicCard extends React.Component {
-  render() {
+  constructor() {
+    super();
+    this.state = ({
+      checked: false,
+      loading: false,
+    });
+    this.onCheckBoxChange = this.onCheckBoxChange.bind(this);
+    this.addRemoveSong = this.addRemoveSong.bind(this);
+  }
+
+  onCheckBoxChange({ target }) {
+    this.setState({ checked: target.checked }, () => this.addRemoveSong());
+  }
+
+  async addRemoveSong() {
     const { music } = this.props;
+    const { checked } = this.state;
+    this.setState({ loading: true });
+    if (checked) {
+      await addSong(music);
+    } else {
+      await removeSong(music);
+    }
+    this.setState({ loading: false });
+  }
+
+  render() {
+    const { checked, loading } = this.state;
+    const { music } = this.props;
+    const { onCheckBoxChange } = this;
     return (
       <div className="cardMusic">
         <p>{ music.trackName }</p>
@@ -15,7 +45,18 @@ class MusicCard extends React.Component {
             audio
           </code>
         </audio>
-        <input type="checkbox" value={ music.trackId } />
+        <label htmlFor={ music.trackId }>
+          <input
+            type="checkbox"
+            name="Favorita"
+            value={ music.trackId }
+            data-testid={ `checkbox-music-${music.trackId}` }
+            id={ music.trackId }
+            checked={ checked }
+            onChange={ onCheckBoxChange }
+          />
+        </label>
+        { loading ? <Loading /> : '' }
       </div>
     );
   }
